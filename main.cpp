356faa6979abff6b7e551b6112a31f9bf9733682
Q.Phu Nguyen <ngqp2k@outlook.com>
2022-08-14 12:13:54 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define all(a) (a).begin(), (a).end()
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	return 0;
+}
